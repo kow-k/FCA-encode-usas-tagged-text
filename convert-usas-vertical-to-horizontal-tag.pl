6a78877862193540f56f29f6f896235e51ac2a78
Kow Kuroda <kow.kuroda@gmail.com>
2022-05-17 15:46:50 +0900

    major revision

--- convert-usas-vertical-to-horizontal-tag.pl
+++ convert-usas-vertical-to-horizontal-tag.pl
@@ -2,29 +2,35 @@
 #
 # developed by Kow Kuroda
 # created on 2022/05/07
+# modified on 2022/05/14
 
+# modules
 use strict;
 use warnings;
 
+# variables
 my $verbose = 0;
 my $sid = 0;
-my $splitter = ' ';
-my $joint = "~";
+my $tag_sep = ' ';
+my $tag_joint = "~";
 
+# main
 while ( <> ) {
-	if ( $verbose ) { print $_; }
-	#
-	if ( $_ =~ /-----/ ) {
+	my $line = $_;
+	if ( $verbose ) { print $line; }
+	# detect a sentence
+	if ( $line =~ /-----/ ) {
 			$sid++;
-			if ($verbose) { printf "s%s begins.\n", $sid; }
+			if ( $verbose ) { printf "s%s begins.\n", $sid; }
 			print "\n";
 	}
-	elsif ( $_ =~ /(\w+)\ +(\w+)\ +(\w+)\ +(['\w]+)\ +([^\n]+)\n/ ) {
-		if ($verbose) { print "$1; $2; $3; $4; $5;\n"; }
+	# parse lines of vertical mode and generate tag concatenations
+	if ( $line =~ /(\w+)\ +(\w+)\ +(\w+)\ +(['\w]+)\ +([^\n]+)\n/ ) {
+		if ($verbose) { print "#parse: $1; $2; $3; $4; $5;\n"; }
 		my $word = $4;
-		my $tagged = $5;
-		$tagged =~ s/[%@]//g; # remove offensive characters
-		printf "%s_%s ", $word, join($joint, split($splitter, $tagged));
+		my $tags = $5;
+		$tags =~ s/[%@]//g; # remove offensive characters
+		printf "%s_%s ", $word, join($tag_joint, split($tag_sep, $tags));
 	}
 }
 
